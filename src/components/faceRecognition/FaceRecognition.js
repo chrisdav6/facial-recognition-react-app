c4192cd8ef7b22a626e6b6474ea83b36292fda5e
chrisdav6 <chris.davis5440@gmail.com>
2018-08-06 16:13:45 -0400

    configured clarifai to recognize a single face

--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
   if(imageUrl !== "") {
     return (
       <div className="faceRecognition">
-        <img src={imageUrl} alt="Face Recognition" />
+        <img id="inputImage" src={imageUrl} alt="Face Recognition" />
+        <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
       </div>
     );
   } else {
